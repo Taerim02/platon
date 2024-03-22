92de479964345ac40da9b2893677f5ba0af1b367
Eli <114699239+Taerim02@users.noreply.github.com>
2024-03-22 18:18:33 +0100

    change import constants.py

--- platon/non-characterization/rds_filter.py
+++ platon/non-characterization/rds_filter.py
@@ -6,7 +6,7 @@ import pyfastx
 import argparse
 import pandas as pd
 import json
-from ..platon.constants import MIN_PROTEIN_IDENTITY
+import platon.constants as pc
 
 parser = argparse.ArgumentParser(description="Process FASTA file for ORF detection") 
 parser.add_argument("fasta_file", help="Input FASTA file")
@@ -73,7 +73,7 @@ with open(args.tsv_file, "r") as fh:
         locus = cols[0].rpartition('_')
         contig_id = locus[0]
         orf_id = locus[2]
-        if((float(cols[2]) >= MIN_PROTEIN_IDENTITY) and (contig_id in contigs)):
+        if((float(cols[2]) >= pc.MIN_PROTEIN_IDENTITY) and (contig_id in contigs)):
             contig = contigs[contig_id]
             orf = contig['orfs'][int(orf_id)]
             orf['protein_id'] = cols[1]
