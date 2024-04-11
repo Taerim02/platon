95d128a97f36013edf43d4215fd23b6d64e9f3b6
Taerim <114699239+Taerim02@users.noreply.github.com>
2024-04-11 13:54:50 +0200

    change the set name as hits_set, not hits

--- platon/characterization/mob.py
+++ platon/characterization/mob.py
@@ -17,7 +17,7 @@ sequence_database_path = str(args.faa_file)
 
 # set the variable needed to store information processed by pyhmmer
 tsv_header = ['contig', 'type', 'orf_id', 'bitscore', 'evalue']
-hits = set()
+hits_set = set()
 
 with pyhmmer.easel.SequenceFile(sequence_database_path, digital=True) as seq_file:
     proteins = seq_file.read_block()
@@ -27,13 +27,13 @@ with pyhmmer.plan7.HMMFile(args.db_path) as hmm_file:
         for hit in hits:
             if hit.included:
                 contig_id, orf_id = hit.name.decode().rsplit('_', 1)
-                if f'{contig_id}_{orf_id}' not in hits:
+                if f'{contig_id}_{orf_id}' not in hits_set:
                     hit_info = {
                         'type': hits.query_name.decode(),
                         'bitscore': hit.score,
                         'evalue': hit.evalue,
                     }
-                    hits.add(f'{contig_id}_{orf_id}')
+                    hits_set.add(f'{contig_id}_{orf_id}')
                     with open(args.output, "a", newline='') as fh:
                         writer = csv.DictWriter(fh, delimiter="\t", fieldnames=tsv_header)
                         is_empty = fh.tell() == 0
